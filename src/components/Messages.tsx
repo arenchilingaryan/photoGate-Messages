578bcc21c1832ab2a9e8b48684c457880846235f
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-12-03 14:00:01 -0800

    Add react-router-dom

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 const Messages = () => {
   return (
-    <div className="div" style={{ fontSize: '100px' }}>
-      Messages
-    </div>
+    <BrowserRouter basename='/messages'>
+      <div className="div" style={{ fontSize: '100px' }}>
+        Messages
+        <Link to="/one">ONE</Link>
+        <Link to="/two">TWO</Link>
+        <Route path="/one" render={() => <div>ONE</div>} />
+        <Route path="/two" render={() => <div>TWO</div>} />
+      </div>
+    </BrowserRouter>
   );
 };
 
